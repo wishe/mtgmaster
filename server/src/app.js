0099fced12c51da1b86ab43bb1cb298af3893db2
Alex Uleberg <alex@webcode.no>
2018-10-17 23:08:12 +0200

    Added production and dev errorhandler, mongoose settings and improvements to app loading

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,59 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
 
+// Configure mongoose promise to global promise
+mongoose.promise = global.Promise;
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+//Init APP
 const app = express();
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.use(cors());
 
-app.get('/status', (req, res) => {
-  res.send({
-    message: 'Hello World',
+app.use(cors());
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'mtgmaster', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+if(!isProduction) {
+  app.use(errorHandler());
+}
+
+// Configure mongoose
+mongoose.connect('');
+mongoose.set('debug', true);
+
+//Error handlers & middlewares
+if(!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
   });
 });
 
 
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 8081, () => console.log('Server running!'));
